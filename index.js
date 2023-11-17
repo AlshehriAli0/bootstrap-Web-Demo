66e0fd50d73c31201aca76c0baf6b89b51a8fbb8
Ali Alshehri <Ali0fawzish@outlook.com>
2023-11-17 19:41:11 +0300

    Add dark mode toggle and update mobile status bar style

--- index.js
+++ index.js
@@ -7,11 +7,17 @@ document
 
 function toggleDarkMode() {
   document.body.classList.toggle("dark-mode");
-}
-
-function toggleDarkMode() {
-  document.body.classList.toggle("dark-mode");
   var navbar = document.getElementById("navbar");
   navbar.classList.toggle("navbar-light");
   navbar.classList.toggle("navbar-dark");
+
+  var metaTag = document.querySelector(
+    'meta[name="apple-mobile-web-app-status-bar-style"]'
+  );
+
+  if (document.body.classList.contains("dark-mode")) {
+    metaTag.setAttribute("content", "black-translucent");
+  } else {
+    metaTag.setAttribute("content", "default");
+  }
 }
